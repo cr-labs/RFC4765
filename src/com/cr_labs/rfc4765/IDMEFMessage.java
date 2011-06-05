9fba304dc43a4468f7322c4ba9217a1cb58a8893
Jim Youll <jim@cr-labs.com>
2011-06-05 13:03:45 -0700

    Documentation cleanup in IDMEFMessage; add /doc and README

--- src/com/cr_labs/rfc4765/IDMEFMessage.java
+++ src/com/cr_labs/rfc4765/IDMEFMessage.java
@@ -25,28 +25,28 @@ Contact: jim@cr-labs.com
  * Implements the IDMEF-Message base class, 4.2.1 of RFC4765, having only the
  * attribute "version"
  * 
- * <p>Subclasses of IDMEF-Message are at this writing, "Alert" and "Heartbeat". As
- * of this writing, the RFC specifies that applications specifying a value for
- * "Version" must specify the value to "1.0". That value is forced here and is
+ * <p>The RFC specifies that applications providing a value for
+ * "Version" must set the value to "1.0". That value is forced here and is
  * not changeable. Class is abstract so that only subclasses can be
  * instantiated, as the spec seems to not anticipate that this class would ever
  * contain more than basic common fields for many message types.
  * 
+ * <p>Subclasses are {@link IDMEFMessage_Alert}, {@link IDMEFMessage_CorrelationAlert},
+ * {@link IDMEFMessage_Heartbeat}, {@link IDMEFMessage_OverflowAlert}, and {@link IDMEFMessage_ToolAlert}.
+ * 
  * <p>
- * All IDMEF messages are instances of the IDMEF-Message class; it is the
- * top-level class of the IDMEF data model, as well as the IDMEF DTD. There are
- * currently two types (subclasses) of IDMEF-Message: Alert and Heartbeat.
+ * All IDMEF messages are instances of the IDMEFMessage class.
  * 
  * 
  * @author jim
- * @version 0.1
+ * @version 0.11
  */
 
 public abstract class IDMEFMessage {
 
 	/**
-	 * As of this writing, the RFC specifies that applications specifying a
-	 * value for "Version" must specify the value to "1.0" so that value is
+	 * As of this writing, the RFC specifies that applications providing a
+	 * value for "Version" must set the value to "1.0", That value is
 	 * forced here and is not changeable.
 	 */
 	public static final String VERSION = "1.0";
